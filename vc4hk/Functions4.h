83feaceda71c6dd0d3f8401ce0c2bf01114c016c
Kevin McKee <kxzxxx@gmail.com>
2016-09-11 23:45:19 +0800

    update

--- vc4hk/Functions4.h
+++ vc4hk/Functions4.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define M_PI 3.14159265359
+#define M_PI 3.14159265359f
 
 DWORD  AimCorrection2(fb::Vec3 MyPosition, fb::Vec3 MyVelocity,
 	const fb::Vec3  EnemyP, fb::Vec3 EnemyVelocity, float  v0, float Gravity, fb::Vec3* out)
@@ -14,14 +14,14 @@ DWORD  AimCorrection2(fb::Vec3 MyPosition, fb::Vec3 MyVelocity,
 		flPitch = 0;
 		x = MyPosition.DistanceToVector(EnemyPosition);
 		out->z = 0;
-
-
+		
+			
 		for (; i <= 5; i++) {
 
 
 
 			time = abs(x / (v0));
-
+			
 			EnemyPosition.x = EnemyP.x + EnemyVelocity.x*time;
 			EnemyPosition.y = EnemyP.y + EnemyVelocity.y*time;
 			EnemyPosition.z = EnemyP.z + EnemyVelocity.z*time;
@@ -31,12 +31,12 @@ DWORD  AimCorrection2(fb::Vec3 MyPosition, fb::Vec3 MyVelocity,
 			x = abs(Driection.VectorLength2());
 
 			tmp = pow(v0, 4) - (Gravity*((Gravity*(x)*(x)) + 2 * (Driection.y)*v0*v0));
-
+			
 			if (tmp <= 0)return 0x1;
 
 			flPitch = atan((v0*v0 - sqrt(tmp)) / (Gravity*x));
 
-
+			return 100;
 			if (EnemyVelocity.x == 0.f&& EnemyVelocity.y == 0.f&&EnemyVelocity.z == 0.f)break;
 
 			if (x <= 5)break;
@@ -54,7 +54,7 @@ DWORD  AimCorrection2(fb::Vec3 MyPosition, fb::Vec3 MyVelocity,
 		Driection = EnemyPosition - MyPosition;
 		//max 0x40002f55
 		//min 
-
+	
 		if (flPitch > 1.4830 || flPitch < -1.221 || _isnan(flPitch))return 0x2;
 
 		//if (flPitch > 1.48350 || flPitch < -1.02200)return 0x2;
@@ -64,15 +64,15 @@ DWORD  AimCorrection2(fb::Vec3 MyPosition, fb::Vec3 MyVelocity,
 
 
 
-		if (flYaw < 0)flYaw = flYaw + 2 * M_PI;
+		
 
-		if (flYaw> 2 * M_PI || flYaw < 0 || _isnan(flYaw))return 0x3;
+		if (flYaw> 2 * M_PI ||  _isnan(flYaw))return 0x3;
 
 
 
-		out->x = flPitch;
+		out->x = flYaw ;
 
-		out->y = flYaw;
+		out->y = flPitch;
 
 
 		return 0x0;
@@ -81,22 +81,21 @@ DWORD  AimCorrection2(fb::Vec3 MyPosition, fb::Vec3 MyVelocity,
 		return 0x4;
 	}
 }
+float DistanceToCrosshair(fb::Vec3 MyPosition, fb::Vec3 EnemyPosition, const fb::SoldierAimingSimulation* aimer) {
 
-double DistanceToCrosshair(fb::Vec3 MyPosition, fb::Vec3 EnemyPosition, const fb::SoldierAimingSimulation* aimer) {
-
-	double fYawDifference, flPitchDifference;
+	float fYawDifference, flPitchDifference;
 
 	fb::Vec3 vDir = EnemyPosition - MyPosition;
 
-	double dist = vDir.VectorLength();
+	float dist = vDir.VectorLength();
 
 
 
 	fYawDifference = -atan2(vDir.x, vDir.z);
 
-	if (fYawDifference < 0)fYawDifference = fYawDifference + 6.2831;
+	if (fYawDifference < 0)fYawDifference = fYawDifference + 2 * M_PI;
 
-	fYawDifference = abs(fYawDifference - aimer->m_Pitch);
+	fYawDifference = abs(fYawDifference - aimer->m_Yaw);
 
 	//if (dist < 10 && fYawDifference < 3.0f)return 0.001;else
 
@@ -111,7 +110,7 @@ double DistanceToCrosshair(fb::Vec3 MyPosition, fb::Vec3 EnemyPosition, const fb
 
 
 
-	flPitchDifference = abs(flPitchDifference - aimer->m_Yaw);
+	flPitchDifference = abs(flPitchDifference - aimer->m_Pitch);
 
 	return abs(dist*cos(flPitchDifference)*sin(fYawDifference));
 }
